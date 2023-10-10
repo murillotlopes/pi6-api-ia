adf23ab527ad8ede4daf5c15c45a84458a8f7b17
murillotlopes <murillo.tlopes@hotmail.com>
2023-10-10 20:34:20 -0300

    iniciando projeto

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from dotenv import load_dotenv
+from os import getenv
+
+from app import routes
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_envvar("FLASK_ENV")
+
+    routes.init_app(app)
+
+    return app
